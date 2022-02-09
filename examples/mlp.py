ff24ded0edc217b62384ee73afd2ba99ec1a44fe
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2022-02-09 11:50:03 +0100

    added mlp multi-fidelity example with conditions

--- examples/mlp.py
+++ examples/mlp.py
@@ -9,40 +9,35 @@ aggressive racing mechanism and successive halving.
 
 MLP is a deep neural network, and therefore, we choose epochs as fidelity type. The digits dataset
 is chosen to optimize the average accuracy on 5-fold cross validation.
+
+This example is adapted from https://github.com/automl/SMAC3/blob/master/examples/python/mlp_mf.py.
 """
+__copyright__ = "Copyright 2022, AutoML.org Freiburg-Hannover"
+__license__ = "3-clause BSD"
 
 import logging
 
 logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 import warnings
 import numpy as np
-
-import ConfigSpace as CS
-from ConfigSpace.hyperparameters import (
-    CategoricalHyperparameter,
-    UniformFloatHyperparameter,
-    UniformIntegerHyperparameter,
-)
 
 from sklearn.datasets import load_digits
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.neural_network import MLPClassifier
 
-from smac.configspace import ConfigurationSpace
-from smac.facade.smac_mf_facade import SMAC4MF
-from smac.scenario.scenario import Scenario
-
-__copyright__ = "Copyright 2021, AutoML.org Freiburg-Hannover"
-__license__ = "3-clause BSD"
+import hydra
+from omegaconf import DictConfig, OmegaConf
 
 
 digits = load_digits()
 
 
 # Target Algorithm
-def mlp_from_cfg(cfg, seed, budget):
+@hydra.main(config_path="configs", config_name="mlp")
+def mlp_from_cfg(cfg):
     """
     Creates a MLP classifier from sklearn and fits the given data on it.
 
@@ -59,6 +54,9 @@ def mlp_from_cfg(cfg, seed, budget):
     -------
     float
     """
+    log.info(OmegaConf.to_yaml(cfg))
+    seed = cfg["seed"]
+    budget = cfg["budget"]
 
     # For deactivated parameters, the configuration stores None-values.
     # This is not accepted by the MLP, so we replace them with placeholder values.
@@ -92,107 +90,4 @@ def mlp_from_cfg(cfg, seed, budget):
 
 
 if __name__ == "__main__":
-    # Build Configuration Space which defines all parameters and their ranges.
-    # To illustrate different parameter types,
-    # we use continuous, integer and categorical parameters.
-    cs = ConfigurationSpace()
-
-    n_layer = UniformIntegerHyperparameter("n_layer", 1, 5, default_value=1)
-    n_neurons = UniformIntegerHyperparameter(
-        "n_neurons", 8, 1024, log=True, default_value=10
-    )
-    activation = CategoricalHyperparameter(
-        "activation", ["logistic", "tanh", "relu"], default_value="tanh"
-    )
-    solver = CategoricalHyperparameter(
-        "solver", ["lbfgs", "sgd", "adam"], default_value="adam"
-    )
-    batch_size = UniformIntegerHyperparameter("batch_size", 30, 300, default_value=200)
-    learning_rate = CategoricalHyperparameter(
-        "learning_rate",
-        ["constant", "invscaling", "adaptive"],
-        default_value="constant",
-    )
-    learning_rate_init = UniformFloatHyperparameter(
-        "learning_rate_init", 0.0001, 1.0, default_value=0.001, log=True
-    )
-
-    # Add all hyperparameters at once:
-    cs.add_hyperparameters(
-        [
-            n_layer,
-            n_neurons,
-            activation,
-            solver,
-            batch_size,
-            learning_rate,
-            learning_rate_init,
-        ]
-    )
-
-    # Adding conditions to restrict the hyperparameter space
-    # Since learning rate is used when solver is 'sgd'
-    use_lr = CS.conditions.EqualsCondition(
-        child=learning_rate, parent=solver, value="sgd"
-    )
-    # Since learning rate initialization will only be accounted for when using 'sgd' or 'adam'
-    use_lr_init = CS.conditions.InCondition(
-        child=learning_rate_init, parent=solver, values=["sgd", "adam"]
-    )
-    # Since batch size will not be considered when optimizer is 'lbfgs'
-    use_batch_size = CS.conditions.InCondition(
-        child=batch_size, parent=solver, values=["sgd", "adam"]
-    )
-
-    # We can also add  multiple conditions on hyperparameters at once:
-    cs.add_conditions([use_lr, use_batch_size, use_lr_init])
-
-    # SMAC scenario object
-    scenario = Scenario(
-        {
-            "run_obj": "quality",  # we optimize quality (alternative to runtime)
-            "wallclock-limit": 100,  # max duration to run the optimization (in seconds)
-            "cs": cs,  # configuration space
-            "deterministic": True,
-            # Uses pynisher to limit memory and runtime
-            # Alternatively, you can also disable this.
-            # Then you should handle runtime and memory yourself in the TA
-            "limit_resources": True,
-            "cutoff": 30,  # runtime limit for target algorithm
-            "memory_limit": 3072,  # adapt this to reasonable value for your hardware
-        }
-    )
-
-    # Max budget for hyperband can be anything. Here, we set it to maximum no. of epochs to train the MLP for
-    max_epochs = 50
-
-    # Intensifier parameters
-    intensifier_kwargs = {"initial_budget": 5, "max_budget": max_epochs, "eta": 3}
-
-    # To optimize, we pass the function to the SMAC-object
-    smac = SMAC4MF(
-        scenario=scenario,
-        rng=np.random.RandomState(42),
-        tae_runner=mlp_from_cfg,
-        intensifier_kwargs=intensifier_kwargs,
-    )
-
-    # Example call of the function with default values
-    # It returns: Status, Cost, Runtime, Additional Infos
-    def_value = smac.get_tae_runner().run(
-        config=cs.get_default_configuration(), budget=max_epochs, seed=0
-    )[1]
-
-    print("Value for default configuration: %.4f" % def_value)
-
-    # Start optimization
-    try:
-        incumbent = smac.optimize()
-    finally:
-        incumbent = smac.solver.incumbent
-
-    inc_value = smac.get_tae_runner().run(config=incumbent, budget=max_epochs, seed=0)[
-        1
-    ]
-
-    print("Optimized Value: %.4f" % inc_value)
+    mlp_from_cfg()
