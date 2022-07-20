b5869e8a4dd0fdb718be2ad162269dcc96013aac
Carolin Benjamins <benjamins@tnt.uni-hannover.de>
2022-07-20 14:28:07 +0200

    Update test

--- tests/test_hydra_smac_sweeper_plugin.py
+++ tests/test_hydra_smac_sweeper_plugin.py
@@ -27,6 +27,9 @@ from hydra_plugins.hydra_smac_sweeper.smac_sweeper import SMACSweeper
 from hydra_plugins.hydra_smac_sweeper.smac_sweeper_backend import SMACSweeperBackend
 from hydra_plugins.hydra_smac_sweeper.search_space_encoding import search_space_to_config_space
 from hydra_plugins.hydra_smac_sweeper.submitit_smac_launcher import SMACLocalLauncher
+from examples.branin import branin
+from smac.facade.smac_ac_facade import SMAC4AC
+from hydra.utils import instantiate, get_class
 
 from smac.facade.smac_hpo_facade import SMAC4HPO
 
@@ -48,13 +51,23 @@ def create_configspace_a() -> ConfigurationSpace:
     return cs
 
 
+def create_configspace_a_with_defaultdefaults() -> ConfigurationSpace:
+    cs = create_configspace_a()
+    cs["x0"].default_value = 0.
+    cs["x1"].default_value = 414.1497313774
+    return cs
+
+
 @mark.parametrize(
     "input, expected",
     [
+        # Test parsing json-Configuration Space
         (
             "tests/configspace_a.json",
             create_configspace_a()
         ),
+
+        # Test creation of ConfigurationSpace from dict
         (
             DictConfig(content={
                 "hyperparameters": {  # same structure as in yaml file
@@ -74,13 +87,47 @@ def create_configspace_a() -> ConfigurationSpace:
                 }
             }),
             create_configspace_a()
+        ),
+
+        # Test passing ConfigurationSpace
+        (
+            create_configspace_a(),
+            create_configspace_a()
+        ),
+
+        # Test if default value is set correctly
+        (
+            DictConfig(content={
+                "hyperparameters": {  # same structure as in yaml file
+                    "x0": {
+                        "type": "uniform_float",
+                        "lower": -512.0,
+                        "upper": 512.0,
+                    },
+                    "x1": {
+                        "type": "uniform_float",
+                        "log": True,
+                        "lower": 335,
+                        "upper": 512.0,
+                    }
+                }
+            }),
+            create_configspace_a_with_defaultdefaults()
         )
     ]
 )
 def test_search_space_parsing(input: Union[str, DictConfig], expected: ConfigurationSpace) -> None:
-    actual = search_space_to_config_space(search_space=input)
+    actual = search_space_to_config_space(search_space=input, seed=48574)
     assert actual == expected
 
+
+def test_search_space_parsing_value_error() -> None:
+    some_list = list()
+    try:
+        cs = search_space_to_config_space(some_list)
+        assert False
+    except ValueError:
+        assert True
 
 @mark.parametrize(
     "kwargs",
@@ -89,11 +136,6 @@ def test_search_space_parsing(input: Union[str, DictConfig], expected: Configura
             content={
                 "smac_class": None,
                 "smac_kwargs": None,
-                # "hydra": {
-                #     "sweep": {
-                #         "dir": "./tmp"
-                #     }
-                # }
             }
         ),
         DictConfig(
@@ -113,14 +155,37 @@ def test_smac_sweeper_args(kwargs: DictConfig):
         n_trials=10,
         n_jobs=1,
         seed=333,
+        smac_kwargs=dict(
+            scenario={
+                "run_obj": "quality",
+                "deterministic": "true",
+        }),
     )
     kwargs.update(default_kwargs)
     # kwargs = OmegaConf.create(kwargs)
     sweeper = SMACSweeperBackend(**kwargs)
-    sweeper.config = kwargs
+    config = OmegaConf.create({
+        "hydra": {
+            "sweep": {
+                "dir": "./tmp"
+            }
+        }
+        }
+    )
+    sweeper.config = config
     sweeper.launcher = SMACLocalLauncher()
+    sweeper.launcher.config = config
+    sweeper.launcher.params['progress'] = "rich"
     sweeper.hydra_context = 1
-    sweeper.sweep(arguments=[])
+    sweeper.task_function = branin
+    smac = sweeper.setup_smac()
+
+    target_smac_class = kwargs.get("smac_class")
+    if target_smac_class is None:
+        assert type(smac) == SMAC4AC
+    else:
+        assert type(smac) == get_class(target_smac_class)
+
     # TODO: test with Scenario kwargs provided
     # TODO: test without Scenario kwargs provided
     # TODO: test with SMAC class provided
